d0d2e362945c4b7a266032052b6dbf812804331f
Tom Standen <standent8@gmail.com>
2023-06-20 16:58:56 +0100

    Cleaned up the setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='db_interfaces',
     version='0.1.0',
-    url='https://github.com/yourusername/your_repository',
-    author='Author Name',
-    author_email='author@gmail.com',
-    description='Description of your package',
-    packages=find_packages(),    
-    install_requires=[],
+    url='https://github.com/Tom-Standen/db_interfaces',
+    author='Tom-Standen',
+    author_email='tstanden3@gmail.com',
+    description='The DB Interfaces library provides a standardized way to interact with Firestore and BigQuery databases in Python projects. The library is designed to be easy to use, flexible, and compatible with modern cloud-based applications.',
+    packages=find_packages(),
+    install_requires=[
+        'google-cloud-firestore>=2.11.1',
+        'pydantic>=1.10.9',
+        'python-dotenv>=1.0.0'
+    ],
 )
